0d8dd5252532af7c662175555c4ceac76355b97d
patrykkondrat <01150244@pw.edu.pl>
2022-08-17 23:06:38 +0200

    add showinfo,makebet, and changebet method

--- db_service.py
+++ db_service.py
@@ -1,10 +1,9 @@
 from typing import List
 from db_init import engine, Bets 
-from sqlalchemy.sql import select, insert
+from sqlalchemy.sql import select, update
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Table
 from tabulate import tabulate
-
+from sqlalchemy.exc import IntegrityError
 con = engine.connect()
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -12,11 +11,23 @@ session = Session()
 # def is_valid_bet(_bet) -> bool:
     # print(Bets.__table__)
 
-def make_bet(user_id, _user, _bet):
-    ins = Bets(id=user_id, user=_user, bet=_bet)
-    session.add(ins)
+def change_bet(_user, _bet):
+    session.query(Bets).filter(Bets.user == _user).update({Bets.bet: _bet})
     session.commit()
 
+def make_bet(user_id, _user, _bet):
+    try:
+        ins = Bets(id=user_id, user=_user, bet=_bet)
+        session.add(ins)
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        # change_bet(_user, _bet)
+        print("Bet in game")
+
+# def add_team(name):
+
+# def winners(bet):
 
 def show_table(plot=True) -> List:
     res = con.execute(select(Bets))
@@ -28,7 +39,6 @@ def show_table(plot=True) -> List:
     return out
 
 if __name__ == "__main__":
-    # is_valid_bet(2)
-    make_bet(3, "Dorota", "1")
-    Bets.__table__
-    print(show_table())
+    make_bet(5, "Dorota5", '2')
+    change_bet("Dorota5", "1")
+    print(show_table(0))
